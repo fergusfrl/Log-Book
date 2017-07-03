a1342e36f8d4b702b599476b1343e529e0a146b3
Fergus Farrell <fergusfrl@gmail.com>
2017-07-04 11:07:14 +1200

    Add files via upload
    Added button functionality in javascript

---
+++ LogBook/js/main.js
@@ -0,0 +1,9 @@
+
+// Clicked add button
+document.getElementById('add').addEventListener('click', function() {
+	var value = document.getElementById('item').value;
+	if (value) {
+		// Log value
+		console.log(value);
+	}
+});
