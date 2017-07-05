2b3ccf5ceca77a41388ed1c9616525c847424f6c
Fergus Farrell <fergusfrl@gmail.com>
2017-07-05 16:35:52 +1200

    Add files via upload
    Updated javascript to include local storage. Can currently add but not delete.

--- LogBook/js/main.js
+++ LogBook/js/main.js
@@ -1,5 +1,10 @@
-
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
+	todo: [],
+	dateList: []
+};
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+
+renderTodoList();
 
 // Clicked add button
 document.getElementById('add').addEventListener('click', function() {
@@ -14,8 +19,40 @@ document.getElementById('add').addEventListener('click', function() {
 	}
 });
 
+function renderTodoList(){
+	if(!data.todo.length){
+		return;
+	}
+	
+	for(var i = 0; i < data.todo.length; i++){
+		var value = data.todo[i];
+		addItemToDOM(value);
+	}
+}
+
+function addItem(value, dateValue){
+	addItemToDOM(value, dateValue);
+	data.todo.push(value);
+	dataObjectUpdated();
+}
+
+function removeItem(){
+	var item = this.parentNode.parentNode;
+	var parent = item.parentNode;
+	var id = parent.id;
+	var value = item.innerText;
+	
+	data.todo.splice(data.todo.indexOf(value), 1);
+	dataObjectUpdated();
+	parent.removeChild(item);
+}
+
+function dataObjectUpdated(){
+	localStorage.setItem('todoList', JSON.stringify(data));
+}
+
 // Add item to ul
-function addItem(text, dateValue){
+function addItemToDOM(text, dateValue){
 	var list = document.getElementById('demo');
 	
 	var item = document.createElement('li');
@@ -42,7 +79,7 @@ function addItem(text, dateValue){
 	item.appendChild(buttons);
 	
 	// Delete item with delete button
-	item.onclick = function() {this.parentNode.removeChild(this);}
+	remove.addEventListener('click', removeItem);
 	
 	list.insertBefore(item, list.childNodes[0]);
 }
